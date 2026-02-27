1d82c5e09b05671906551ff753077bb96342b38b
Mara Schmitman <mara@schmitman.com>
2026-02-27 12:52:06 -0300

    fix: add Bondy font families and color tokens to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bondy: {
-          orange: '#F47C20',
-          dark: '#1A1A2E',
-        }
-      }
+        'b-black': '#111111',
+        'b-off': '#F9F8F6',
+        'b-orange': '#E05C00',
+        'b-mid': '#888885',
+        'b-light': '#D8D6D2',
+        'b-rule': '#EBEBEB',
+        'b-white': '#FFFFFF',
+      },
+      fontFamily: {
+        'display': ['Playfair Display', 'Georgia', 'serif'],
+        'mono-bondy': ['DM Mono', 'monospace'],
+        'body': ['DM Sans', 'system-ui', 'sans-serif'],
+      },
     },
   },
   plugins: [],
